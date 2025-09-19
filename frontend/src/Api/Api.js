67f975776afaece668eed449ce18ba6fb584c6d6
shivanijwork <shivanijwork@gmail.com>
2025-09-19 13:20:45 +0530

    first commit

--- frontend/src/Api/Api.js
+++ frontend/src/Api/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL =  "http://localhost:5000/api";
+const API_URL =  "https://backend-ecom-1-1vlc.onrender.com/api";
 
 function getToken() {
     if (typeof window !== 'undefined') {
